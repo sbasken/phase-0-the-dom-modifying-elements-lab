f42477ce53cf2344c980c11746545ff324ba6cab
Saki <sbasken@gmail.com>
2023-01-08 18:16:18 -0800

    commit tree aa5c2ff9d1f2519b98ba33bdfd9b987646bfada6

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector("main").remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "Saki is the champion"
